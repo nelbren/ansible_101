cff72cb19dc430b50d1d65892a61b5216b4da7fc
Martin Cuellar <nelbren@gmail.com>
2022-09-05 19:56:05 +0100

    Separación de archivos, uso de roles y scripts

---
+++ scripts/ansible_alias.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+alias ansible_ping="/etc/ansible/ansible_101/scripts/ansible_ping.bash"
+alias ansible_playbook="/etc/ansible/ansible_101/scripts/ansible_playbook.bash"
---
+++ scripts/ansible_playbook.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook /etc/ansible/ansible_101/playbook_debian.yml
